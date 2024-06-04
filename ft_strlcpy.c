6d4c7cbf9e96ea87d2ed1be728bd39f4f814df32
Omar Lengua <omarns21@gmail.com>
2024-06-04 23:52:55 +0200

    agregada función strlcpy a la librería

---
+++ ft_strlcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olengua- <olengua-@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 23:29:09 by olengua-          #+#    #+#             */
+/*   Updated: 2024/06/04 23:39:26 by olengua-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i + 1 < size && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	if (size != 0)
+		dst[i] = '\0';
+	return (ft_strlen(src));
+}
