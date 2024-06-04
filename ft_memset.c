ab2925c6ad292cd6aad17b779db44c819c8b5761
Omar Lengua <omarns21@gmail.com>
2024-06-04 13:00:41 +0200

    agregadas funciones strlen, memset y corregido typos

---
+++ ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olengua- <olengua-@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 12:41:16 by olengua-          #+#    #+#             */
+/*   Updated: 2024/06/04 12:46:09 by olengua-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*a;
+	size_t			i;
+
+	a = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		a[i] = (unsigned char)c;
+		i++;
+	}
+	return (s);
+}
