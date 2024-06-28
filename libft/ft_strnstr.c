f0d725abc6f68cb326ff69e50ff61ba7d418f456
Omar Lengua <omarns21@gmail.com>
2024-06-28 23:53:10 +0200

    agregada función strnstr

---
+++ libft/ft_strnstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olengua- <olengua-@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/28 23:42:43 by olengua-          #+#    #+#             */
+/*   Updated: 2024/06/28 23:52:34 by olengua-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	c;
+	size_t	n_len;
+	char	*hay;
+
+	i = 0;
+	hay = (char *)big;
+	n_len = ft_strlen(little);
+	if (n_len == 0 || haystack == little)
+		return (hay);
+	while (hay[i] != '\0' && i < len)
+	{
+		c = 0;
+		while (hay[i + c] != '\0' && little[c] != '\0'
+			&& hay[i + c] == little[c] && i + c < len)
+			c++;
+		if (c == n_len)
+			return (hay + i);
+		i++;
+	}
+	return (0);
+}
