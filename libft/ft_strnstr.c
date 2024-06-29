4772e1c3bf37262192bed06fe72b3bac3690752b
Omar Lengua <omarns21@gmail.com>
2024-06-29 13:01:58 +0200

    agregada función strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: olengua- <olengua-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 23:42:43 by olengua-          #+#    #+#             */
-/*   Updated: 2024/06/28 23:58:54 by olengua-         ###   ########.fr       */
+/*   Updated: 2024/06/29 12:58:24 by olengua-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
+	char	*strbig;
+	size_t	lenmatch;
+	size_t	litlen;
 	size_t	i;
-	size_t	c;
-	size_t	n_len;
-	char	*hay;
 
 	i = 0;
-	hay = (char *)big;
-	n_len = ft_strlen(little);
-	if (n_len == 0 || big == little)
-		return (hay);
-	while (hay[i] != '\0' && i < len)
+	strbig = (char *)big;
+	litlen = ft_strlen(little);
+	if (litlen == 0 || big == little)
+		return (strbig);
+	while (strbig[i] != '\0' && i < len)
 	{
-		c = 0;
-		while (hay[i + c] != '\0' && little[c] != '\0'
-			&& hay[i + c] == little[c] && i + c < len)
-			c++;
-		if (c == n_len)
-			return (hay + i);
+		lenmatch = 0;
+		while (strbig[i + lenmatch] != '\0' && little[lenmatch] != '\0'
+			&& strbig[i + lenmatch] == little[lenmatch] && i + lenmatch < len)
+			lenmatch++;
+		if (lenmatch == litlen)
+			return (strbig + i);
 		i++;
 	}
 	return (0);
