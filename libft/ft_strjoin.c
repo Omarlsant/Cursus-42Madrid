b141f73bbddaca0de7ef30a61171104e99b9e287
Omar Lengua <omarns21@gmail.com>
2024-07-11 09:22:29 +0200

    agregada la función strjoin

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olengua- <olengua-@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/09 09:56:01 by olengua-          #+#    #+#             */
+/*   Updated: 2024/07/11 09:02:59 by olengua-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (str == 0)
+		return (0);
+	ft_strlcpy(str, s1, ft_strlen(s1) + 1);
+	ft_strlcat(str, s2, ft_strlen(s2) + 1);
+	return (str);
+}
