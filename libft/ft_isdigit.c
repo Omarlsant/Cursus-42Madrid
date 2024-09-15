5cfb1c85a38f029b81f1b3d077c876c2d3611890
Omar Lengua <omarns21@gmail.com>
2024-09-15 15:11:41 +0200

    He agregado la función ft_isdigit

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -10,7 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+//#include "libft.h"
+#include <stdio.h>
 
 int	ft_isdigit(int c)
 {
@@ -19,4 +20,24 @@ int	ft_isdigit(int c)
 		return (1);
 	}
 	return (0);
+}
+
+// Esta es una función que comprueba si un carácter es un dígito (0-9).
+
+int main()
+{
+	char test_chars[] = {'A', 'z', '1', '*', '5', 'n'};
+	int i = 0;
+	int array_size = sizeof(test_chars) / sizeof(test_chars[0]);
+
+    while (i < array_size) {
+        if (ft_isdigit(test_chars[i])) {
+            printf("'%c' es un dígito.\n", test_chars[i]);
+        } else {
+            printf("'%c' no es un dígito.\n", test_chars[i]);
+        }
+        i++;
+    }
+
+    return 0;
 }
