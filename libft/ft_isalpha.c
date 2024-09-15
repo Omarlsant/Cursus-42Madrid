bafdf3c54f4df300e30ca2848fa0c9b1f929ada6
Omar Lengua <omarns21@gmail.com>
2024-09-15 15:10:55 +0200

    He agregado la función ft_isalpha

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: olengua- <olengua-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 09:55:38 by olengua-          #+#    #+#             */
-/*   Updated: 2024/06/05 20:03:52 by olengua-         ###   ########.fr       */
+/*   Updated: 2024/09/14 16:43:29 by olengua-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,3 +20,22 @@ int	ft_isalpha(int c)
 	}
 	return (0);
 }
+
+/*Esta es una función que comprueba si un caractér es una letra del alfabeto.
+
+int main() {
+    char test_chars[] = {'A', 'z', '1', '*', 'M', 'n'};
+    int i = 0;
+    int array_size = sizeof(test_chars) / sizeof(test_chars[0]);
+
+    while (i < array_size) {
+        if (ft_isalpha(test_chars[i])) {
+            printf("'%c' es una letra.\n", test_chars[i]);
+        } else {
+            printf("'%c' no es una letra.\n", test_chars[i]);
+        }
+        i++;
+    }
+
+    return 0;
+}*/
