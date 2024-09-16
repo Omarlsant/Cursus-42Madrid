1db18b2530331fa15561f20bcb7b3bfdc86afca1
Omar Lengua <omarns21@gmail.com>
2024-09-16 21:31:44 +0200

    He agregado la función strlen

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+//#include <stdio.h>
 
 size_t	ft_strlen(const char *s)
 {
@@ -23,3 +24,28 @@ size_t	ft_strlen(const char *s)
 	}
 	return (i);
 }
+
+// This function is used to calculate the length of a string.
+
+/*int main(void)
+{
+	const char *str1 = "Hola, mundo!";
+	const char *str2 = "";
+	const char *str3 = "1234567890";
+	const char *str4 = "¡Hola! ¿Cómo estás?";
+	const char *str5 = "C es un lenguaje de programación.";
+
+	size_t len1 = ft_strlen(str1);
+	size_t len2 = ft_strlen(str2);
+	size_t len3 = ft_strlen(str3);
+	size_t len4 = ft_strlen(str4);
+	size_t len5 = ft_strlen(str5);
+
+	printf("La longitud de la cadena \"%s\" es: %zu\n", str1, len1);
+	printf("La longitud de la cadena \"%s\" es: %zu\n", str2, len2);
+	printf("La longitud de la cadena \"%s\" es: %zu\n", str3, len3);
+	printf("La longitud de la cadena \"%s\" es: %zu\n", str4, len4);
+	printf("La longitud de la cadena \"%s\" es: %zu\n", str5, len5);
+
+	return 0;
+}*/
