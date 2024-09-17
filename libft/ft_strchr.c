3bd198fef6159b92c7a36e2c7898b49e65395e54
Omar Lengua <omarns21@gmail.com>
2024-09-18 00:41:34 +0200

    He agregado la función strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+//#include <stdio.h>
 
 char	*ft_strchr(const char *s, int c)
 {
@@ -20,10 +21,38 @@ char	*ft_strchr(const char *s, int c)
 	while (*s != '\0')
 	{
 		if (*s == character)
-			return ((character *)s);
+			return ((char *)s);
 		s++;
 	}
 	if (character == '\0')
-		return ((character *)s);
+		return ((char *)s);
 	return (0);
 }
+
+/* This function is used to find the first occurrence of 
+   a character in a string */
+
+/*int main() {
+    const char str[] = "Hola, Copilot!";
+    char ch = 'i';
+    char *result;
+    const char str2[] = "Hola, Copilot!";
+    char chr = 'u';
+    char *result2;
+
+    result = ft_strchr(str, ch);
+    result2 = ft_strchr(str2, chr);
+
+    if (result != NULL) {
+        printf("El carácter '%c' se encontró en la cadena: %s\n", ch, str);
+    } else {
+        printf("El carácter '%c' no se encontró en la cadena.\n", ch);
+    }
+
+    if (result2 != NULL) {
+        printf("El carácter '%c' se encontró en la cadena: %s\n", chr, str2);
+    } else {
+        printf("El carácter '%c' no se encontró en la cadena.\n", chr);
+    }
+    return 0;
+}*/
