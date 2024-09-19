3582cc6b09ea81306837509c3ba2a4b53bfa8c80
Omar Lengua <omarns21@gmail.com>
2024-09-19 23:58:14 +0200

    He agregado la función substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -11,6 +11,9 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -35,3 +38,57 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	str[i] = 0;
 	return (str);
 }
+
+/* This function is used to extract a substring from a 
+   main string. Obtain a substring from the string ‘s’, 
+   starting at the index ‘start’ and with a maximum 
+   length of ‘len’ characters.*/
+
+/*int main() {
+    const char *str = "Hello, world!";
+    char *substr1 = ft_substr(str, 7, 5);
+    char *substr2 = ft_substr(str, 0, 5);
+    char *substr3 = ft_substr(str, 13, 5);
+
+    printf("Original string: '%s'\n", str);
+    printf("Substring (start=7, len=5): '%s'\n", substr1);
+    printf("Substring (start=0, len=5): '%s'\n", substr2);
+    printf("Substring (start=13, len=5): '%s'\n", substr3);
+
+    free(substr1);
+    free(substr2);
+    free(substr3);
+    return 0;
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*str;
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	len = ft_strlen(s);
+	str = malloc((len + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}*/
