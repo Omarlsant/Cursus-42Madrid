571f3c72262191d4ea03bcd6b33a094d6b66aefe
Omar Lengua <omarns21@gmail.com>
2024-10-23 17:41:35 +0200

    Implementation: Added ft printf_h

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -10,4 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
+# include <stdlib.h>
+# include <stdarg.h>
+# include <unistd.h>
+
+int     ft_printf(const char *str, ...);
+int     ft_putnbr_base(long n, char *base, int lenbase);
+int     ft_putchar(char c);
+int     ft_strlen_int(const char *str);
+int     ft_putstr(char *s);
+int     ft_putnbr_base_p(long n, char *base, int lenbase);
+
+#endif
