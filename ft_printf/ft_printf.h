88dddf50825c19ad5a9cce5bafcbbb69a6cde989
Omar Lengua <omarns21@gmail.com>
2024-10-23 18:25:26 +0200

    Implementation: printf.h

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: olengua- <olengua-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 18:37:49 by olengua-          #+#    #+#             */
-/*   Updated: 2024/10/06 18:37:51 by olengua-         ###   ########.fr       */
+/*   Updated: 2024/10/23 18:22:04 by olengua-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-int     ft_printf(const char *str, ...);
-int     ft_putnbr_base(long n, char *base, int lenbase);
-int     ft_putchar(char c);
-int     ft_strlen_int(const char *str);
-int     ft_putstr(char *s);
-int     ft_putnbr_base_p(long n, char *base, int lenbase);
+int	ft_printf(const char *str, ...);
+int	ft_putnbr_base(long n, char *base, int lenbase);
+int	ft_putchar(char c);
+int	ft_strlen_int(const char *str);
+int	ft_putstr(char *s);
+int	ft_putnbr_base_p(long n, char *base, int lenbase);
 
 #endif
